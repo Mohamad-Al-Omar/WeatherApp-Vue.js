33377d3ece3fe5e9f83cb7861767fe679c698772
Mohamad Adnan Alomar <m.al.omar94@gmail.com>
2021-07-04 16:45:25 +0400

    import i18n library

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'ar',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
