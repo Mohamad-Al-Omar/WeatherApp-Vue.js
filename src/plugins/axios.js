5f6483cf63a5d47e9351a47e4fcb9bd73c35f677
Mohamad Adnan Alomar <m.al.omar94@gmail.com>
2021-07-04 16:51:38 +0400

    config axios with base url and token

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,11 +5,11 @@ import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+ axios.defaults.headers.common['Authorization'] = "c50004d24015a979a7e5a7e84355b95d";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
+   baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
