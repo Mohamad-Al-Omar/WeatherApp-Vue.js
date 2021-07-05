fbffa7650407a56e51cbd5d5fd7de99752a7228d
Mohamad Adnan Alomar <m.al.omar94@gmail.com>
2021-07-05 09:57:13 +0400

    connect with open weather api

--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -5,20 +5,23 @@
         <h1>Weather App</h1>
       </v-col>
       <v-col cols="12" align="center">
-        <v-col cols="12" md="8">
-          <v-text-field
-            v-model="queryText"
-            :placeholder="Placeholder"
-            outlined
-            dense
-          />
+        <v-col cols="12" md="6">
+          <v-form @submit.prevent="fetchData">
+            <v-text-field
+              v-model="queryText"
+              :placeholder="Placeholder"
+              outlined
+              dense
+          /></v-form>
         </v-col>
       </v-col>
       <v-col cols="12" align="center">
         <v-col cols="12" md="4" sm="6">
           <v-card>
             <v-card-text>{{ cityName }}</v-card-text>
-            <v-card-title class="justify-center" > {{temperature}}&#8451; </v-card-title>
+            <v-card-title class="justify-center">
+              {{ temperature }}&#8451;
+            </v-card-title>
           </v-card>
         </v-col>
       </v-col>
@@ -36,6 +39,25 @@ export default {
       cityName: "City Name",
       temperature: 40,
     };
+  },
+  methods: {
+    fetchData() {
+      console.log(this.$axios.baseURL);
+      this.$axios
+        .get(
+            "weather?q=" +
+            this.queryText +
+            "&appid=c50004d24015a979a7e5a7e84355b95d"
+        )
+        .then((res) => {
+          window.res = res;
+          console.log("yes " + res);
+        })
+        .catch((err) => {
+          window.err = err;
+          console.log("No " + err);
+        });
+    },
   },
   watch: {
     queryText: function (newQuery) {
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,12 +4,12 @@ import Vue from 'vue';
 import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
- axios.defaults.headers.common['Authorization'] = "c50004d24015a979a7e5a7e84355b95d";
+axios.defaults.baseURL = "https://api.openweathermap.org/data/2.5/";
+// axios.defaults.headers.common['Authorization'] = "c50004d24015a979a7e5a7e84355b95d";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-   baseURL: process.env.baseURL || process.env.apiUrl || ""
+  // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
@@ -17,11 +17,11 @@ let config = {
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  function(config) {
+  function (config) {
     // Do something before request is sent
     return config;
   },
-  function(error) {
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
   }
@@ -29,17 +29,17 @@ _axios.interceptors.request.use(
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function(response) {
+  function (response) {
     // Do something with response data
     return response;
   },
-  function(error) {
+  function (error) {
     // Do something with response error
     return Promise.reject(error);
   }
 );
 
-Plugin.install = function(Vue) {
+Plugin.install = function (Vue) {
   Vue.axios = _axios;
   window.axios = _axios;
   Object.defineProperties(Vue.prototype, {
